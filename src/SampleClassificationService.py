d289707743bdc81dc37256ba375a1dfd3edeadf4
vikranth22446 <rama22446@gmail.com>
2021-01-26 17:49:43 -0800

    Inital commit with shopping files

--- src/SampleClassificationService.py
+++ src/SampleClassificationService.py
@@ -3,6 +3,7 @@ from sclog import getLogger
 
 from plp.proto import Classification_pb2
 from plp.proto import Classification_pb2_grpc
+from src.shopping_classifier.shopping_skill import ShoppingSkill
 
 logger = getLogger(__name__)
 
@@ -12,13 +13,20 @@ CLASSIFICATION_SERVICE_PORT = 50060
 class SampleClassificationService(
     Classification_pb2_grpc.ClassificationServiceServicer
 ):
+    def __init__(self) -> None:
+        super().__init__()
+        self.skill = ShoppingSkill()
+
     def ClassifyText(self, request, context):
         logger.debug("received request %i to classify %s", request.id, request.text)
+        skill_label, skill_prob = self.skill.classify(request.text)
 
         # Here we'd actually run the classifier
-        result_classification = "oos"
-        result_confidence = 1.0
+        result_classification = skill_label #"oos"
+        result_confidence = skill_prob #1.0
         result_extras = "fake classification"
+        if skill_label != "oos":
+            result_extras = self.skill.find_shopping_item(request.text) # "fake classification"
 
         return Classification_pb2.ClassificationResponse(
             classifierName="sample",
