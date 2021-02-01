788df4f992d0ae02dfb398b789a32b4904bac9ef
vikranth22446 <rama22446@gmail.com>
2021-01-31 20:48:18 -0800

    Add General Classifier Since the skills themselves are pretty abstracted, this was a simple change.
    This could probably abstracted to another class.

--- src/GeneralClassificationService.py
+++ src/GeneralClassificationService.py
@@ -3,28 +3,31 @@ from sclog import getLogger
 
 from plp.proto import Classification_pb2
 from plp.proto import Classification_pb2_grpc
-from general_classifier.general_classifier import GeneralClassifierSkill
+from shopping_classifier.shopping_skill import ShoppingSkill
 
 logger = getLogger(__name__)
 
-CLASSIFICATION_SERVICE_PORT = 50060
+CLASSIFICATION_SERVICE_PORT = 50061
 
 
-class SampleClassificationService(
+class GeneralClassificationService(
     Classification_pb2_grpc.ClassificationServiceServicer
 ):
     def __init__(self) -> None:
         super().__init__()
-        self.skill = GeneralClassifierSkill()
+        self.skill = ShoppingSkill()
 
     def ClassifyText(self, request, context):
-        logger.debug("received genera classification request %i to classify %s", request.id, request.text)
+        logger.debug("received request %i to classify %s", request.id, request.text)
         skill_label, skill_prob = self.skill.classify(request.text)
 
         # Here we'd actually run the classifier
         result_classification = skill_label #"oos"
         result_confidence = skill_prob #1.0
-        result_extras = ""
+        result_extras = "fake classification"
+        if skill_label != "oos":
+            _, prob, shopping_item = self.skill.find_shopping_item(request.text)
+            result_extras = f"Shopping item found: {shopping_item} with prob {prob}" 
         return Classification_pb2.ClassificationResponse(
             classifierName="sample",
             classification=result_classification,
